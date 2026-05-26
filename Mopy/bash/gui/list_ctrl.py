336a37eb73cc812805d22fd15eb9963675114498
eypowxoa <nepozitron@gmail.com>
2026-05-26 20:51:27 +0400

    Fix for lists scrolling on linux

--- Mopy/bash/gui/list_ctrl.py
+++ Mopy/bash/gui/list_ctrl.py
@@ -94,6 +94,36 @@ class _DragListCtrl(_wx.ListCtrl, ListCtrlAutoWidthMixin):
         self.fnDropFiles = fnDropFiles
         self.fnDropIndexes = fnDropIndexes
         self.fnDndAllow = fnDndAllow
+        self._scroll_offset: int | None = None
+
+    def _get_scroll_offset(self) -> int:
+        offset = self._scroll_offset
+        if (offset is None) and (self.GetItemCount() > 0):
+            offset = self.GetItemRect(0).height
+            self._scroll_offset = offset
+        return offset or 0
+
+    def LineUp(self):
+        # Doesn't work on Linux, as described in https://docs.wxpython.org/wx.Window.html
+        # So use ScrollList instead.
+        # Quote from there:
+        # LineUp
+        # … Same as ScrollLines (-1).
+        # ScrollLines
+        # … This function is currently only implemented under MSW and wx.TextCtrl
+        # under wxGTK (it also works for wx.Scrolled classes under all platforms).
+        return self.ScrollList(0, -self._get_scroll_offset())
+
+    def LineDown(self):
+        # Doesn't work on Linux, as described in https://docs.wxpython.org/wx.Window.html
+        # So use ScrollList instead.
+        # Quote from there:
+        # LineDown
+        # … Same as ScrollLines (1).
+        # ScrollLines
+        # … This function is currently only implemented under MSW and wx.TextCtrl
+        # under wxGTK (it also works for wx.Scrolled classes under all platforms).
+        return self.ScrollList(0, self._get_scroll_offset())
 
     def OnDragging(self,x,y,dragResult):
         # We're dragging, see if we need to scroll the list
